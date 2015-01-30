f0816d544747a05ddb7b5a8c504d4ebc19a06016
Romboide82 <ugo98.juve@libero.it>
2015-01-30 23:34:16 +0100

    hitbox

--- src/main/Lobby.java
+++ src/main/Lobby.java
@@ -8,8 +8,10 @@ public class Lobby {
 	
 	public Lobby(){
 		mapSelection = new Rectangle(855,0,100,45);
-		lobbyHB = new Rectangle[1];
+		lobbyHB = new Rectangle[3];
 		lobbyHB[0] = new Rectangle(0,0,1018,45);
+		lobbyHB[1] = new Rectangle(419,0,5,198);
+		lobbyHB[2] = new Rectangle(73,198,350,45);
 	}
 
 	public Rectangle getMapSelection() {
