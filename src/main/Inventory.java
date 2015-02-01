c26d84b662e9d4976a1006581d4c8ec374465ad8
Giampli <france.marco@gmail.com>
2015-02-01 04:01:21 +0100

    ignore

--- src/main/Inventory.java
+++ src/main/Inventory.java
@@ -42,8 +42,8 @@ public class Inventory {
 		return dropNumber;
 	}	
 	
-	public ArrayList<Integer> getDropName() {
-		return dropNumber;
+	public ArrayList<String> getDropName() {
+		return dropName;
 	}
 
 	public int getX() {
