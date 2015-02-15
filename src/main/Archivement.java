b73eab31454eade7a5c5d380adc5136d5d8aaa11
Romboide82 <ugo98.juve@libero.it>
2015-02-15 12:34:59 +0100

    m
    k

--- src/main/Archivement.java
+++ src/main/Archivement.java
@@ -4,16 +4,28 @@ import java.awt.Point;
 
 public class Archivement {
 	
-	String name;
+	String name, goal;
 	Point imagePosition;
 	boolean unlocked;
 	
 	
-	public Archivement(String name, Point imagePosition){
+	public Archivement(String name, String goal, Point imagePosition){
 		this.name = name;
+		this.goal = goal;
 		this.imagePosition = imagePosition;
 		unlocked = false;
+	}
+
+
+	public boolean isUnlocked() {
+		return unlocked;
+	}
+
+
+	public void setUnlocked(boolean unlocked) {
+		this.unlocked = unlocked;
 	}	
+	
 	
 	
 }
--- src/main/ArchivementList.java
+++ src/main/ArchivementList.java
@@ -6,23 +6,33 @@ import java.util.ArrayList;
 public class ArchivementList {
 	ArrayList<Archivement> archivement = new ArrayList<Archivement>();;
 	ArrayList<String> names = new ArrayList<String>();
+	ArrayList<String> goals = new ArrayList<String>();
 	ArrayList<Point> imagePositions = new ArrayList<Point>();
 	
 	public ArchivementList(){
 		UpdateListNames();
+		UpdateListPoint();
 		UpdateListPoint();
 		Load();
 	}
 	
 	void Load(){
 		for(int i = 0; i < names.size(); i++){
-			archivement.add(new Archivement(names.get(i), imagePositions.get(i)));
+			archivement.add(new Archivement(names.get(i), goals.get(i), imagePositions.get(i)));
 		}
-		
+	}
+	
+	public void CheckUnlockedArchivement(int enemyKilled){
+		if(enemyKilled >= 10)
+			archivement.get(0).setUnlocked(true);
 	}
 	
 	public void UpdateListNames(){
-		names.add("Kill 10 enemies");
+		names.add("Begginer Zombie Killer");
+	}
+	
+	public void UpdateListGoals(){
+		goals.add("Kill 10 enemies");
 	}
 	
 	public void UpdateListPoint(){
